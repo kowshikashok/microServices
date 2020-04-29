b6b46dc9c7d99c60d178e6604278a2ea67bbf369
Ashok <Kowshik.Ashok@standardbank.co.za>
2020-04-29 17:21:08 +0200

    Initial commit

--- src/main/java/com/javainuse/microServices/MicroServicesApplication.java
+++ src/main/java/com/javainuse/microServices/MicroServicesApplication.java
@@ -2,12 +2,20 @@ package com.javainuse.microServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class MicroServicesApplication {
+public class MicroServicesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServicesApplication.class, args);
 	}
-
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(MicroServicesApplication.class);
+	  }
+	
 }
